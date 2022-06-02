b3a342d29ea06e2bf72de2fbe743dd1cb32c9331
aoyama <aoyama@val.co.jp>
2022-06-02 13:25:35 +0900

    solved?のspecが通るようになった

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -6,8 +6,9 @@ class Solver
 end
 
 class Board
-  BLANK = 0
+  BLANK = 0 # board.txtで"."で表されるマスは空白。データ上では数値の0で表現する。
   SIZE = 9
+  BLOCK_SIZE = SIZE / 3
 
   def initialize(board_filename)
     @data = []
@@ -49,8 +50,27 @@ class Board
     column(i).reject { |x| x == BLANK }.sort.uniq
   end
 
+  def used_numbers_in_block(n)
+    nums = []
+    top_left_i = (n % BLOCK_SIZE) * BLOCK_SIZE
+    top_left_j = (n / BLOCK_SIZE) * BLOCK_SIZE
+    (0...3).each do |i|
+      (0...3).each do |j|
+        num = at(top_left_i + i, top_left_j + j)
+        nums << num if num != BLANK
+      end
+    end
+    nums.sort.uniq
+  end
+
   def not_used_numbers(used_numbers)
     (1..SIZE).to_a - used_numbers
+  end
+
+  def solved?
+    (0...SIZE).all? { |i| used_numbers_in_row(i).length == SIZE } &&
+    (0...SIZE).all? { |i| used_numbers_in_column(i).length == SIZE } &&
+    (0...SIZE).all? { |i| used_numbers_in_block(i).length == SIZE}
   end
 end
 
--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -16,9 +16,34 @@ describe 'Sudoku'do
       end
     end
 
+    describe '#used_numbers_in_block' do
+      it '指定されたブロックで使われている数の配列が昇順で返る' do
+        expect(board.used_numbers_in_block(4)).to eq [1, 2, 3, 5, 7, 8]
+      end
+
+      it '指定されたブロック(5)で使われている数の配列が昇順で返る' do
+        expect(board.used_numbers_in_block(5)).to eq [2, 3, 4, 6, 9]
+      end
+    end
+
     describe '#not_used_numbers' do
       it '指定された列で使われている数の配列が昇順で返る' do
         expect(board.not_used_numbers([3, 1, 4])).to eq [2, 5, 6, 7, 8, 9]
+      end
+    end
+
+    describe '#solved?' do
+      context 'ボードが正解でない状態の場合' do
+        it 'falseが返る' do
+          expect(board.solved?).to eq false
+        end
+      end
+
+      context 'ボードが正解の状態の場合' do
+        let(:board) { Board.new('./data/solved.txt') }
+        it 'trueが返る' do
+          expect(board.solved?).to eq true
+        end
       end
     end
   end
