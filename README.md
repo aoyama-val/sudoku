c7ee10f4840fef812a74c32445ae742f13fb3f0f
aoyama <aoyama@val.co.jp>
2022-06-02 14:09:56 +0900

    Solverがちゃんと正解を出せるようになった

---
+++ README.md
@@ -0,0 +1,19 @@
+# 数独ソルバー
+
+## セットアップ
+
+```
+$ bundle install
+```
+
+## 実行方法
+
+```
+$ bundle exec ruby ./lib/sudoku.rb
+```
+
+## rpsec実行方法
+
+```
+$ bundle exec rspec ./spec/sudoku_spec.rb
+```
--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,7 +1,40 @@
 class Solver
-  def solve(board_filename)
-    @board = Board.new(board_filename)
-    @board.print
+  # def initialize(board_filename)
+  #   @board = Board.new(board_filename)
+  #   @board.print
+  # end
+
+  def solve(board)
+    return if board.full?
+
+    (0...9).each do |j|
+      (0...9).each do |i|
+        if board.at(i, j) == Board::BLANK
+          used_numbers = board.used_numbers_in_row(j) + board.used_numbers_in_column(i) + board.used_numbers_in_same_block(i, j)
+          candidates = board.not_used_numbers(used_numbers)
+          # print "Can put #{i}, #{j}: "
+          # p candidates
+          return false if candidates.empty?
+          candidates.each do |num|
+            cloned = board.clone
+            cloned.put(i, j, num)
+            puts
+            cloned.print
+            if cloned.solved?
+              puts
+              cloned.print
+              puts "Solved!"
+              return true
+            end
+            if solve(cloned)
+              return true
+            end
+          end
+          return false
+        end
+      end
+    end
+    return false
   end
 end
 
@@ -9,6 +42,8 @@ class Board
   BLANK = 0 # board.txtで"."で表されるマスは空白。データ上では数値の0で表現する。
   SIZE = 9
   BLOCK_SIZE = SIZE / 3
+
+  attr_accessor :data
 
   def initialize(board_filename)
     @data = []
@@ -18,6 +53,13 @@ class Board
       end
     end
     validate!
+  end
+
+  def clone
+    old_data = @data.clone
+    cloned = super
+    cloned.data = old_data
+    cloned
   end
 
   def print
@@ -30,8 +72,21 @@ class Board
     raise "InvalidBoardError length=#{@data.length}" if @data.length != SIZE ** 2
   end
 
+  # ブロック番号を返す
+  def block(i, j)
+    i / BLOCK_SIZE + (j / BLOCK_SIZE) * BLOCK_SIZE
+  end
+
+  def used_numbers_in_same_block(i, j)
+    used_numbers_in_block(block(i, j))
+  end
+
   def at(i, j)
     @data[j * SIZE + i]
+  end
+
+  def put(i, j, num)
+    @data[j * SIZE + i] = num
   end
 
   def row(i)
@@ -72,6 +127,12 @@ class Board
     (0...SIZE).all? { |i| used_numbers_in_column(i).length == SIZE } &&
     (0...SIZE).all? { |i| used_numbers_in_block(i).length == SIZE}
   end
+
+  def full?
+    @data.all? { |num| num != Board::BLANK }
+  end
 end
 
-Solver.new.solve('data/board.txt')
+if $0 == __FILE__
+  Solver.new.solve(Board.new('data/board.txt'))
+end
