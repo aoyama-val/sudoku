ea83daa6b7f2b2bd7d2b178092b1a1775103df13
aoyama <aoyama@val.co.jp>
2022-06-02 10:23:25 +0900

    lib配下へ移動

---
+++ lib/sudoku.rb
@@ -0,0 +1,58 @@
+class Solver
+  def solve(board_filename)
+    @board = Board.new(board_filename)
+    @board.print
+    p @board.row(0)
+    p @board.used_numbers_in_row(0)
+    p @board.column(0)
+    p @board.used_numbers_in_column(0)
+
+  end
+end
+
+class Board
+  BLANK = 0
+  SIZE = 9
+
+  def initialize(board_filename)
+    @data = []
+    open(board_filename) do |f|
+      f.each do |line|
+        @data += line.chomp.split('').map(&:to_i)
+      end
+    end
+    validate!
+  end
+
+  def print
+    @data.each_slice(SIZE) do |slice|
+      puts slice.map { |num| num == BLANK ? '. ' : "#{num} " }.join('')
+    end
+  end
+
+  def validate!
+    raise "InvalidBoardError length=#{@data.length}" if @data.length != SIZE ** 2
+  end
+
+  def at(i, j)
+    @data[j * SIZE + i]
+  end
+
+  def row(i)
+    (0...SIZE).map { |j| at(j, i)}
+  end
+
+  def column(i)
+    (0...SIZE).map { |j| at(i, j)}
+  end
+
+  def used_numbers_in_row(i)
+    row(i).reject { |x| x == BLANK }.sort.uniq
+  end
+
+  def used_numbers_in_column(i)
+    column(i).reject { |x| x == BLANK }.sort.uniq
+  end
+end
+
+Solver.new.solve('data/board.txt')
